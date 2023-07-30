d97ce4a11bb1c00f3f813d67f76ebab3493a8c65
minji Kong <minzyaaaaaa@gmail.com>
2023-07-30 19:31:00 +0900

    Refactor: Logo 컴포넌트 수정

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -2,24 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-  width?: string;
   height?: string;
+  fontSize?: string;
 }
 
-export default function Logo({ width, height }: Props) {
+export default function Logo({ height, fontSize }: Props) {
   return (
-    <ImageWrapper width={width} height={height}>
-      <Image src='/logo.svg' alt='logo' />
-    </ImageWrapper>
+    <LogoWrapper height={height} fontSize={fontSize}>
+      <img className='logo_image' src='/logo.jpg' alt='logo' />
+      <p className='logo_text'>기발한 사람들</p>
+    </LogoWrapper>
   );
 }
 
-const ImageWrapper = styled.div<{ width?: string; height?: string }>`
-  width: ${(props) => props.width || '110px'};
-  height: ${(props) => props.height || 'auto'};
-`;
+const LogoWrapper = styled.div<{ height?: string; fontSize?: string }>`
+  display: flex;
+  align-items: center;
+  height: ${(props) => props.height || '22px'};
 
-const Image = styled.img`
-  width: '100%';
-  height: '100%';
+  img {
+    margin-right: 4px;
+    width: ${(props) => props.height || '22px'};
+    height: 100%;
+    border-radius: 4px;
+  }
+
+  p {
+    color: #353535;
+    font-size: ${(props) => props.fontSize || '18px'};
+    font-weight: 700;
+    white-space: nowrap;
+    letter-spacing: -1.8px;
+  }
 `;
