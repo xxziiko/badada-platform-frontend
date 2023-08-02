f4772a7c7c26b3314840b5f73d7477175cd93afb
minji Kong <minzyaaaaaa@gmail.com>
2023-08-03 00:29:42 +0900

    Add: api 폴더 절대경로

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
+      '@api': path.resolve(__dirname, 'src/api'),
       '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@pages': path.resolve(__dirname, 'src/pages'),
