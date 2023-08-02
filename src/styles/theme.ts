5c861db5640f2b293998d1d4844684528ae68508
minji Kong <minzyaaaaaa@gmail.com>
2023-08-03 00:31:12 +0900

    Add: animation

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -36,7 +36,7 @@ export const fontSize = {
 export const shadow = {
   default: '0px 2px 4px 0px rgba(0, 0, 0, 0.15)',
   page: '0px 0px 8px 4px rgba(0, 0, 0, 0.05)',
-  input: '0px 0px 4px 0px rgba(0, 0, 0, 0.15',
+  input: '0px 0px 4px 0px rgba(0, 0, 0, 0.15)',
 };
 
 export const animation = {
